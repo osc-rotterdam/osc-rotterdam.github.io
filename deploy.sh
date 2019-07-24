1606bc81fab8ccdcf41a8482da62d9e6a98e2c23
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2019-07-24 18:04:26 +0200

    development branch

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "Deploying updates to GitHub...\n"
+
+# make folder for master brach and move it to tmp
+cp CNAME public/
+cp README.md public/
+TMPDIR=$(dirname $(mktemp))
+cp -rav public/ $TMPDIR/
+
+# switch to master branch
+git checkout master
+
+# replace public folder
+rm * -rf
+mv $TMPDIR/public/* ./
+
+
+# add changes in public to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push --all
+
+# switch back to development branch
+git checkout development
